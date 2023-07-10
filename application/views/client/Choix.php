a94f2fe1d18e14cb062f0cfb02bb1c5b2fdf63c1
TsikyAro <119358772+TsikyAro@users.noreply.github.com>
2023-07-10 23:04:28 +0300

    choix

--- application/views/client/Choix.php
+++ application/views/client/Choix.php
@@ -1,88 +1,137 @@
-<section class="py-0">
+<section class="py-4 overflow-hidden">
 
     <div class="container">
+        <div class="col-lg-6 text-center mx-auto mt-6">
+            <h5 class="fw-bold text-danger fs-3 fs-lg-5 lh-sm my-2">Je veux ...</h5>
+        </div>
         <div class="row h-100 gx-2 mt-7">
-            <div class="col-sm-6 col-lg-3 mb-3 mb-md-0 h-100 pb-4">
-                <div></div>
-                <div class="row justify-content g-0"></div>
-                <div class="card card-span h-100">
-                    <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="assets/img/gallery/discount-item-1.png" alt="..." />
-                        <div class="card-actions">
-                            <div class="badge badge-foodwagon bg-primary p-4">
-                                <div class="d-flex flex-between-center">
-                                    <div class="text-white fs-7">1-7</div>
-                                    <div class="d-block text-white fs-2">jpurs <br />
-                                        <div class="fw-normal fs-1 mt-2">Off</div>
+
+            <div class="col-md-6">
+                <div class="col-lg-6 text-center mx-auto mb-3 mb-md-5 mt-4">
+                    <h4>Perdre</h4>
+                </div>
+                <div class="row h-100 gx-2 mt-7">
+                    <div class="col-sm-6 col-lg-6 mb-3 mb-md-0 h-100 pb-4">
+                        <div class="card card-span h-100">
+                            <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="<?= base_url('assets/img/gallery/discount-item-2.png')?>" alt="..." />
+                                <div class="card-actions">
+                                    <div class="badge badge-foodwagon bg-primary p-4">
+                                        <div class="d-flex flex-between-center">
+                                            <div class="text-white fs-7">1 a 7</div>
+                                            <div class="d-block text-white fs-2">Kg<br />
+                                                <div class="fw-normal fs-1 mt-2">-33%</div>
+                                            </div>
+                                        </div>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="card-body px-0">
+                                <div class="col-md-4 choixRegime">
+                                    <h5 class="fw-bold text-1000 text-truncate">1 semaine</h5>
+                                    <span class="badge bg-soft-danger py-2 px-3">
+                                        <span class="fs-1 text-danger">$144</span>
+                                    </span>
+                                </div>
+                                <div class="col-md-3 choixRegime">
+                                    <a class="btn btn-lg btn-danger" href="#!" role="button">Choisir</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="card-body px-0">
-                        <h5 class="fw-bold text-1000 text-truncate">Flat Hill Slingback</h5>
-                        <span class="badge bg-soft-danger py-2 px-3">
-                            <span class="fs-1 text-danger">6 days Remaining</span>
-                        </span>
-                    </div><a class="stretched-link" href="#"></a>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3 mb-3 mb-md-0 h-100 pb-4">
-            <div class="card card-span h-100">
-                <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="assets/img/gallery/discount-item-2.png" alt="..." />
-                <div class="card-actions">
-                    <div class="badge badge-foodwagon bg-primary p-4">
-                    <div class="d-flex flex-between-center">
-                        <div class="text-white fs-7">10</div>
-                        <div class="d-block text-white fs-2">% <br />
-                        <div class="fw-normal fs-1 mt-2">Off</div>
+
+                    <div class="col-sm-6 col-lg-6 mb-3 mb-md-0 h-100 pb-4">
+                        <div class="card card-span h-100">
+                            <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="<?= base_url('assets/img/gallery/discount-item-1.png')?>" alt="..." />
+                                <div class="card-actions">
+                                    <div class="badge badge-foodwagon bg-primary p-4">
+                                        <div class="d-flex flex-between-center">
+                                            <div class="text-white fs-7">7 a 10</div>
+                                            <div class="d-block text-white fs-2">Kg<br />
+                                                <div class="fw-normal fs-1 mt-2">-37%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body px-0">
+                                <div class="col-md-4 choixRegime">
+                                    <h5 class="fw-bold text-1000 text-truncate">1 mois</h5>
+                                    <span class="badge bg-soft-danger py-2 px-3">
+                                        <span class="fs-1 text-danger">$139</span>
+                                    </span>
+                                </div>
+                                <div class="col-md-3 choixRegime">
+                                    <a class="btn btn-lg btn-danger" href="#!" role="button">Choisir</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
                 </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="col-lg-6 text-center mx-auto mb-3 mb-md-5 mt-4">
+                    <h4>Gagner</h4>
                 </div>
-                <div class="card-body px-0">
-                <h5 class="fw-bold text-1000 text-truncate">Ocean Blue Ring</h5><span class="badge bg-soft-danger py-2 px-3"><span class="fs-1 text-danger">6 days Remaining</span></span>
-                </div><a class="stretched-link" href="#"></a>
-            </div>
-            </div>
-            <div class="col-sm-6 col-lg-3 mb-3 mb-md-0 h-100 pb-4">
-            <div class="card card-span h-100">
-                <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="assets/img/gallery/discount-item-3.png" alt="..." />
-                <div class="card-actions">
-                    <div class="badge badge-foodwagon bg-primary p-4">
-                    <div class="d-flex flex-between-center">
-                        <div class="text-white fs-7">25</div>
-                        <div class="d-block text-white fs-2">% <br />
-                        <div class="fw-normal fs-1 mt-2">Off</div>
+                <div class="row h-100 gx-2 mt-7">
+                    <div class="col-sm-6 col-lg-6 mb-3 mb-md-0 h-100 pb-4">
+                        <div class="card card-span h-100">
+                            <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="<?= base_url('assets/img/gallery/discount-item-3.png')?>" alt="..." />
+                                <div class="card-actions">
+                                    <div class="badge badge-foodwagon bg-primary p-4">
+                                        <div class="d-flex flex-between-center">
+                                            <div class="text-white fs-7">1 a 4</div>
+                                            <div class="d-block text-white fs-2">Kg<br />
+                                                <div class="fw-normal fs-1 mt-2">-32%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body px-0">
+                                <div class="col-md-4 choixRegime">
+                                    <h5 class="fw-bold text-1000 text-truncate">1 semaine</h5>
+                                    <span class="badge bg-soft-danger py-2 px-3">
+                                        <span class="fs-1 text-danger">$152</span>
+                                    </span>
+                                </div>
+                                <div class="col-md-3 choixRegime">
+                                    <a class="btn btn-lg btn-danger" href="#!" role="button">Choisir</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
-                </div>
-                </div>
-                <div class="card-body px-0">
-                <h5 class="fw-bold text-1000 text-truncate">Brown Leathered Wallet</h5><span class="badge bg-soft-danger py-2 px-3"><span class="fs-1 text-danger">6 days Remaining</span></span>
-                </div><a class="stretched-link" href="#"></a>
-            </div>
-            </div>
-            <div class="col-sm-6 col-lg-3 mb-3 mb-md-0 h-100 pb-4">
-            <div class="card card-span h-100">
-                <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="assets/img/gallery/discount-item-4.png" alt="..." />
-                <div class="card-actions">
-                    <div class="badge badge-foodwagon bg-primary p-4">
-                    <div class="d-flex flex-between-center">
-                        <div class="text-white fs-7">20</div>
-                        <div class="d-block text-white fs-2">% <br />
-                        <div class="fw-normal fs-1 mt-2">Off</div>
+        
+                    <div class="col-sm-6 col-lg-6 mb-3 mb-md-0 h-100 pb-4">
+                        <div class="card card-span h-100">
+                            <div class="position-relative"> <img class="img-fluid rounded-3 w-100" src="<?= base_url('assets/img/gallery/discount-item-4.png')?>" alt="..." />
+                                <div class="card-actions">
+                                    <div class="badge badge-foodwagon bg-primary p-4">
+                                        <div class="d-flex flex-between-center">
+                                            <div class="text-white fs-7">4 a 6</div>
+                                            <div class="d-block text-white fs-2">Kg<br />
+                                                <div class="fw-normal fs-1 mt-2">-38%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body px-0">
+                                <div class="col-md-4 choixRegime">
+                                    <h5 class="fw-bold text-1000 text-truncate">1 mois</h5>
+                                    <span class="badge bg-soft-danger py-2 px-3">
+                                        <span class="fs-1 text-danger">$147</span>
+                                    </span>
+                                </div>
+                                <div class="col-md-3 choixRegime">
+                                    <a class="btn btn-lg btn-danger" href="#!" role="button">Choisir</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-                </div>
-                <div class="card-body px-0">
-                <h5 class="fw-bold text-1000 text-truncate">Silverside Wristwatch</h5><span class="badge bg-soft-danger py-2 px-3"><span class="fs-1 text-danger">6 days Remaining</span></span>
-                </div><a class="stretched-link" href="#"></a>
             </div>
-            </div>
+
         </div>
     </div>
 
