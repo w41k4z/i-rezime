ece6fd8da8dc7cc3038a6559793eb23186b2fe4c
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-07-11 09:48:51 +0300

    update

--- application/views/client/EmploiDuTemps.php
+++ application/views/client/EmploiDuTemps.php
@@ -7,7 +7,11 @@
                         <h5 class="fw-bold text-danger fs-3 fs-lg-5 lh-sm my-2">Organisation du suivie de votre regime</h4>
                     </div>
                     <div class="row">
+<<<<<<< Updated upstream
                         <div class="col-md-8">
+=======
+                        <div class="col-md-6">
+>>>>>>> Stashed changes
                             <div class="content table-responsive table-full-width">
                                 <table class="table table-striped">
                                     <thead>
@@ -63,8 +67,71 @@
                                 </table>
                             </div>
                         </div>
+<<<<<<< Updated upstream
                         <div class="col-md-4">
                             <p class="mb-md-0 fs-1 mx-5">Voici l'emploi du temps que vous devriez suivre pour pouvoir atteindre votre objectif</p><br>
+=======
+                        <div class="col-md-6">
+                            <p class="mb-md-0 fs-1 mx-5">Voici l'emploi du temps que vous devriez suivre pour pouvoir atteindre votre objectif</p><br>
+                            <div class="container">
+                                <!-- Section Tittle -->
+                                <div class="row d-flex justify-content-center">
+                                    <div class="col-lg-6">
+                                        <div class="section-tittle text-center">
+                                            <h5>Les activites sportives<br>a faire!</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Section caption -->
+                                <div class="row">
+                                    <div class="col-xl-4 col-lg-4 col-md-4">
+                                        <div class="services-caption active text-center mb-30">
+                                            <div class="service-icon">
+                                                <span class="flaticon-pay"></span>
+                                            </div> 
+                                            <div class="service-cap">
+                                                <h4>Facile</h4>
+                                                <ul>
+                                                    <li>pont</li>
+                                                    <li>pompe</li>
+                                                    <li>flection</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    <div class="col-xl-4 col-lg-4 col-md-4">
+                                        <div class="services-caption active text-center mb-30">
+                                            <div class="service-icon">
+                                                <span class="flaticon-pay"></span>
+                                            </div> 
+                                            <div class="service-cap">
+                                                <h4>Moyen</h4>
+                                                <ul>
+                                                    <li>pont</li>
+                                                    <li>pompe</li>
+                                                    <li>flection</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    <div class="col-xl-4 col-lg-4 col-md-4">
+                                        <div class="services-caption text-center mb-30">
+                                            <div class="service-icon">
+                                                <span class="flaticon-plane"></span>
+                                            </div> 
+                                            <div class="service-cap">
+                                                <h4>Difficile</h4>
+                                                <ul>
+                                                    <li>pont</li>
+                                                    <li>pompe</li>
+                                                    <li>flection</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+>>>>>>> Stashed changes
                         </div>
                     </div>
                 </div>
